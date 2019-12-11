1bb05499c6dd3dd12f96ef48b989e8603e6abe81
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-12-11 14:34:59 -0600

    Update and rename sshCmd.groovy to ssh.groovy

--- vars/ssh.groovy
+++ vars/ssh.groovy
@@ -1,11 +1,27 @@
+/*
+ssh()
+*/
+
 //import org.asyla.somelibname
 
 def call(Map stepParams = [:]) {
+  
   def credentialID = stepParams.credentialID // unique identifier within Jenkins credental store
   def remoteHost = stepParams.remoteHost // DNS name or IP of the remote ssh server
-  def CMD = stepParams.CMD ?: "hostname"
+  def cmdLine = stepParams.cmdLine ?: "hostname" // Command to remotely execute:  run "hostname" by default
 
   withCredentials([sshUserPrivateKey(credentialsId: credentialID, keyFileVariable: 'keyFileLocation', passphraseVariable: '', usernameVariable: 'userID')]) {
-    returnCode = sh(script: 'ssh ${userID}@'+remoteHost+' -i ${keyFileLocation} -o StrictHostKeyChecking=no '+CMD, returnStatus: true)
+    
+    try {
+      cmdOutput = sh(
+        script: 'ssh ${userID}@'+remoteHost+' -i ${keyFileLocation} -o StrictHostKeyChecking=no '+cmdLine,
+        returnStdout: true
+      ).trim()
+      
+    } catch (Exception ex) {
+      return ("ERROR: ${ex}")
+      
+    }
+    return cmdOutput
   }
 }
