1e97bce2469b5d03a35e129a69a223a074f6b48f
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-12-12 20:07:29 -0600

    Update ssh.groovy

--- vars/ssh.groovy
+++ vars/ssh.groovy
@@ -23,25 +23,20 @@ def call(Map defaults = [:]) {
   def remoteHost = defaults.remoteHost // DNS name or IP of the remote ssh server
   def cmdLine = defaults.cmdLine ?: "hostname" // Command to remotely execute:  run "hostname" by default
   
-  echo "one"
-  echo credentialID
-  echo remoteHost
-  echo cmdLine
+  // echo credentialID
+  // echo remoteHost
+  // echo cmdLine
   
   withCredentials([sshUserPrivateKey(credentialsId: credentialID, keyFileVariable: 'keyFileLocation', passphraseVariable: '', usernameVariable: 'userID')]) {
-    echo "two"
+
     try {
-      echo "about to run ssh"
       cmdOutput = sh(
         script: 'ssh ${userID}@'+remoteHost+' -i ${keyFileLocation} -o StrictHostKeyChecking=no '+cmdLine,
         returnStdout: true
       ).trim()
-      echo "ssh done"
-    } catch (err) { 
-      echo "error"
-      return err.getMessage() 
-    }
-    echo cmdOutput
+      
+    } catch (err) { return err.getMessage() }
+
     return cmdOutput
   }
 }
