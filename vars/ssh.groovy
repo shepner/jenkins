82b8984490a545b06ce08b7f3a9ce09517ad21d6
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-12-12 19:36:39 -0600

    Update ssh.groovy

--- vars/ssh.groovy
+++ vars/ssh.groovy
@@ -22,19 +22,18 @@ def call(Map stepParams = [:]) {
   def credentialID // unique identifier within Jenkins credental store
   def remoteHost // DNS name or IP of the remote ssh server
   def cmdLine = stepParams.cmdLine ?: "hostname" // Command to remotely execute:  run "hostname" by default
-
-  echo "this is a test"
   
   withCredentials([sshUserPrivateKey(credentialsId: credentialID, keyFileVariable: 'keyFileLocation', passphraseVariable: '', usernameVariable: 'userID')]) {
     
     try {
+      echo "about to run ssh"
       cmdOutput = sh(
         script: 'ssh ${userID}@'+remoteHost+' -i ${keyFileLocation} -o StrictHostKeyChecking=no '+cmdLine,
         returnStdout: true
       ).trim()
-      
+      echo "ssh done"
     } catch (err) { return err.getMessage() }
-    
+    echo cmdOutput
     return cmdOutput
   }
 }
