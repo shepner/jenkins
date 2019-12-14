7d9d395da90e297113f4b92b29c7fe65daf6f3f0
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-12-13 19:06:48 -0600

    Update ssh-waitForHost.groovy

--- vars/ssh-waitForHost.groovy
+++ vars/ssh-waitForHost.groovy
@@ -18,22 +18,39 @@ stage('ssh-waitForHost.groovy example') {
 //import org.asyla.somelibname
 
 def call(Map defaults = [:]) {
-  
+
   String credentialID  = defaults.credentialID // unique identifier within Jenkins credental store
   String remoteHost = defaults.remoteHost // DNS name or IP of the remote ssh server
-  String waitTime = defaults.waitTime ?: 10 // time in minutes
+  
+  int waitTime = defaults.waitTime ?: 10 // max time to wait in minutes before giving up
+  int sleepTime = 30 // time in seconds
   
   String result = null
-                    
+  Boolean status = true
+  
   try {
-      result = ssh(
-          credentialID: credentialID,
-          remoteHost: remoteHost,
-          cmdLine: 'sudo reboot'
-      )
-  } catch (def err) {
-      echo "ssh session terminated abnormally (as expected)"
-  }
+    sleep (sleepTime) // lets not be too hasty here
+    
+    timeout (time: waitTime, unit: 'MINUTES') { // keep trying for this amount of time
 
-  return "`ssh_reboot()` complete"
+      while (status == true) { // do we need to keep looping?
+        status = false // be optimistic and assume this will be successful
+
+        try {
+          result = ssh(credentialID: credentialID, remoteHost: sshHost, cmdLine: 'uptime -s')
+        }
+
+        catch (def ssh_err) { // not able to connect
+          status = true // nope, it wasnt successful.  Try again
+          echo 'ssh_err='+ssh_err.getMessage()
+          sleep (sleepTime)
+        }
+      }
+      return result
+    }
+  }
+  catch (def timeout_err) {
+    echo 'timeout_err='+timeout_err.getMessage()
+    throw timeout_err
+  }
 }
