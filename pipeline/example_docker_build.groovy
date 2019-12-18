9b70ec03aed9881b4c8a76c828cf1353e1cec2d4
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-12-18 10:45:33 -0600

    Update example_docker_build.groovy

--- pipeline/example_docker_build.groovy
+++ pipeline/example_docker_build.groovy
@@ -1,20 +1,42 @@
 //https://www.edureka.co/community/55640/jenkins-docker-docker-image-jenkins-pipeline-docker-registry
 
 pipeline {
-
   environment {
-    registry = "docker_hub_account/repository_name"
+    registry = "gustavoapolinario/docker-test"
     registryCredential = 'dockerhub'
+    dockerImage = ''
   }
   
-  agent any  
-
+  agent any
+  
   stages {
-    stage('Building image') {
+    stage('Cloning Git') {
       steps {
+        git 'https://github.com/gustavoapolinario/microservices-node-example-todo-frontend.git'
+      }
+    }
+    
+    stage('Building image') {
+      steps{
         script {
-          docker.build registry + ":$BUILD_NUMBER"
+          dockerImage = docker.build registry + ":$BUILD_NUMBER"
         }
+      }
+    }
+    
+    stage('Deploy Image') {
+      steps{
+        script {
+          docker.withRegistry( '', registryCredential ) {
+            dockerImage.push()
+          }
+        }
+      }
+    }
+    
+    stage('Remove Unused docker image') {
+      steps{
+        sh "docker rmi $registry:$BUILD_NUMBER"
       }
     }
   }
